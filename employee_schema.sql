a141936c8d378dd1c8336831c22e97846af55d15
kimsang-lov <kimsang.mok01@gmail.com>
2024-01-29 07:13:35 +0700

    initial commit

---
+++ employee_schema.sql
@@ -0,0 +1,8 @@
+SELECT * FROM employee;
+
+INSERT INTO employee (first_name, last_name, email) VALUES
+('Leslie','Andrews','leslie@gmail.com'),
+('Emma','Baumgarten','emma@gmail.com'),
+('Avani','Gupta','avani@gmail.com'),
+('Yuri','Petrov','yuri@gmail.com'),
+('Juan','Vega','juan@gmail.com')
