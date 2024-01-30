ff490c2f54d4fcbfa1267c836be873049402ab10
kimsang-lov <kimsang.mok01@gmail.com>
2024-01-30 22:01:28 +0700

    use spring rest data

--- src/main/java/com/kimsang/restcrud/dao/EmployeeRepository.java
+++ src/main/java/com/kimsang/restcrud/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.kimsang.restcrud.dao;
 
 import com.kimsang.restcrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
